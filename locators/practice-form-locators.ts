2506436e549fbea1ec9ef02159e63e460bc119e0
Zeke <ezequiel.dellaechia@gmail.com>
2024-02-23 12:22:43 -0300

    In process

---
+++ locators/practice-form-locators.ts
@@ -0,0 +1,8 @@
+export const PracticeFormLocators = {
+    firstNameBox :  "#firstName",
+    lastNameBox : "#lastName",
+    email: "userEmail",
+    genderMale: "#gender-radio-1",
+    genderFemale: "#gender-radio-2",
+    genderOther: "#gender-radio-3",
+}
---
+++ pages/pratice-form-page.ts
@@ -0,0 +1,45 @@
+import { expect, type Locator, type Page} from "@playwright/test"
+
+import { PracticeFormLocators } from "../locators/practice-form-locators";
+
+
+export class PracticeFormPage{
+
+    readonly page: Page;
+    readonly firstNameBox :  Locator;
+    readonly lastNameBox : Locator;
+    readonly email: Locator;
+    readonly genderMale: Locator;
+    readonly genderFemale: Locator;
+    readonly genderOther: Locator;
+
+
+    constructor(page:Page){
+        this.page = page;
+        this.firstNameBox = page.locator(PracticeFormLocators.firstNameBox)
+        this.email = page.locator(PracticeFormLocators.email)
+        this.genderMale = page.locator(PracticeFormLocators.genderMale)
+        this.genderFemale = page.locator(PracticeFormLocators.genderFemale)
+        this.genderOther = page.locator(PracticeFormLocators.genderOther)
+    }
+
+
+    async visitPracticeFormPage() {
+        await this.page.goto('https://demoqa.com/automation-practice-form');   
+      }
+    
+      async typeFirstName(){
+        await this.firstNameBox.fill("Test")    
+      }
+
+
+
+
+
+
+
+
+
+
+
+}
